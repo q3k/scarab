d807a0339178bfe813cdb8b6d2a0812514312620
Sergiusz Bazanski <q3k@q3k.org>
2019-07-05 15:37:56 +0200

    js: render argument fields

--- js/app.js
+++ js/app.js
@@ -26,6 +26,7 @@ const State = Object.freeze({
         IDLE: Symbol("IDLE"),
         CREATE_JOB_SELECT_TYPE: Symbol("CREATE_JOB_SELECT_TYPE"),
         CREATE_JOB_INPUT_PARAMETERS: Symbol("CREATE_JOB_INPUT_PARAMETERS"),
+        CREATE_JOB_START: Symbol("CREATE_JOB_START"),
 });
 
 let store = {
@@ -33,6 +34,7 @@ let store = {
         state: State.IDLE,
 
         jobTypes: {},
+        creatingJobName: "",
     },
 
     remote: new Remote(),
@@ -44,9 +46,16 @@ let store = {
         let descriptions = await this.remote.descriptions();
         let jobs = descriptions.getJobsList();
         for (let job of jobs) {
-            this.state.jobTypes[job.getName()] = job.getDescription();
+            this.state.jobTypes[job.getName()] = job;
         }
         this.state.state = State.CREATE_JOB_SELECT_TYPE;
+    },
+    jobInputParameters: async function(jobName) {
+        if (jobName === "") {
+            return;
+        }
+        this.state.creatingJobName = jobName;
+        this.state.state = State.CREATE_JOB_INPUT_PARAMETERS;
     },
 };
 
@@ -64,6 +73,9 @@ Vue.component('vbutton', {
 });
 
 Vue.component('modal-job-create', {
+    data: () => { return {
+        'selected': "",
+    }; },
     props: {
         "jobTypes": {type: Object, default: {}},
     },
@@ -71,10 +83,70 @@ Vue.component('modal-job-create', {
     <div id="modal">
         <div id="modalContent">
             <h3>Select Job type...</h3>
-            <select id="select">
-                <option v-for="(description, name) in jobTypes">{{ description }}</option>
+            <select id="select" v-model="selected">
+                <option disabled value="">Select...</option>
+                <option v-for="(job, name) in jobTypes" :value="name">{{ job.getDescription() }}</option>
             </select>
-            <vbutton red :s="{ marginRight: 0, }">OK</vbutton>
+            <vbutton v-on:click="$emit('ok', selected)" red :s="{ marginRight: 0, }">OK</vbutton>
+            <vbutton v-on:click="$emit('close')">Cancel</vbutton>
+        </div>
+    </div>
+    `,
+});
+
+Vue.component('modal-job-input-parameters', {
+    data: () => { return {
+        fields: {},
+    }; },
+    props: {
+        "jobTypes": {type: Object, default: {}},
+        "jobName": {type: String, default: ""},
+    },
+    methods: {
+        getArguments: function() {
+            let res = [];
+            
+            for (let argument of this.jobTypes[this.jobName].getArgumentsList()) {
+                let arg = {
+                    name: argument.getName(),
+                    description: argument.getDescription(),
+                    checkbox: argument.getType() === common_pb.ArgumentDefinition.Type.TYPE_BOOL,
+                    mustBeSet: false,
+                };
+                for (let validator of argument.getValidatorList()) {
+                    if (validator == common_pb.ArgumentDefinition.Validator.VALIDATOR_MUST_BE_SET) {
+                        arg.mustBeSet = true;
+                    }
+                }
+                res.push(arg);
+            }
+
+            return res;
+        }
+    },
+    template: `
+    <div id="modal">
+        <div id="modalContent">
+            <h3>{{ jobTypes[jobName].getDescription() }} ...</h3>
+            <div class="fields">
+                <template v-for="argument in getArguments()">
+                    <label :for=argument.name>
+                        {{ argument.description }}<span v-if="argument.mustBeSet" style="color: red;"> *</span>:
+                    </label>
+                    <input
+                        v-if="argument.checkbox"
+                        v-model=fields[argument.name]
+                        type="checkbox"
+                        :name=argument.name
+                    />
+                    <input
+                        v-else
+                        v-model=fields[argument.name]
+                        :name=argument.name
+                    />
+                </template>
+            </div>
+            <vbutton v-on:click="$emit('ok', fields)" red :s="{ marginRight: 0, }">Create Job</vbutton>
             <vbutton v-on:click="$emit('close')">Cancel</vbutton>
         </div>
     </div>
@@ -89,10 +161,15 @@ let vm = new Vue({
     methods: {
         idle: () => store.idle(),
         jobSelect: () => store.jobSelect(),
+        jobInputParameters: (jobName) => store.jobInputParameters(jobName),
+        jobStart: (fields) => console.log(fields),
     },
     computed: {
         showCreateJobSelectType: function() {
             return this.state.state == State.CREATE_JOB_SELECT_TYPE;
-        }
+        },
+        showCreateJobInputParameters: function() {
+            return this.state.state == State.CREATE_JOB_INPUT_PARAMETERS;
+        },
     },
 });
